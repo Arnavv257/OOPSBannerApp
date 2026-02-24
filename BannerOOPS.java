e6f141f0279cdf05296592ab1b20518202bd3c89
Arnavv257 <ag2053@srmist.edu.in>
2026-02-24 08:53:56 +0530

    coded uc2 to print banner app statements

---
+++ BannerOOPS.java
@@ -0,0 +1,11 @@
+public class BannerOOPS {
+    public static void main(String[] args) {
+        System.out.println("  OOOOO   OOOOO   PPPPP    SSSSS ");
+        System.out.println(" O     O O     O  P    P  S     S");
+        System.out.println(" O     O O     O  P    P  S      ");
+        System.out.println(" O     O O     O  PPPPP    SSSSS ");
+        System.out.println(" O     O O     O  P            S ");
+        System.out.println(" O     O O     O  P      S     S ");
+        System.out.println("  OOOOO   OOOOO   P       SSSSS  ");
+    }
+}
