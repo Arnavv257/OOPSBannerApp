aa8e12c2ef9beb579091040c31358999a0a03ed4
Arnavv257 <ag2053@srmist.edu.in>
2026-03-03 09:20:27 +0530

    coded to do uc6

---
+++ StaticFunctions.java
@@ -0,0 +1,64 @@
+public class StaticFunctions {
+
+    // Helper method for letter O
+    public static String[] buildO() {
+        return new String[]{
+                String.join("", "  *****  "),
+                String.join("", " *     * "),
+                String.join("", "*       *"),
+                String.join("", "*       *"),
+                String.join("", "*       *"),
+                String.join("", " *     * "),
+                String.join("", "  *****  ")
+        };
+    }
+
+    // Helper method for letter P
+    public static String[] buildP() {
+        return new String[]{
+                String.join("", "*******  "),
+                String.join("", "*      * "),
+                String.join("", "*      * "),
+                String.join("", "*******  "),
+                String.join("", "*        "),
+                String.join("", "*        "),
+                String.join("", "*        ")
+        };
+    }
+
+    // Helper method for letter S
+    public static String[] buildS() {
+        return new String[]{
+                String.join("", "  *****  "),
+                String.join("", " *     * "),
+                String.join("", "*        "),
+                String.join("", "  *****  "),
+                String.join("", "        *"),
+                String.join("", " *     * "),
+                String.join("", "  *****  ")
+        };
+    }
+
+    public static void main(String[] args) {
+
+        // Array initialization via methods
+        String[] O = buildO();
+        String[] P = buildP();
+        String[] S = buildS();
+
+        String[] banner = {
+                O[0] + " " + O[0] + " " + P[0] + " " + S[0],
+                O[1] + " " + O[1] + " " + P[1] + " " + S[1],
+                O[2] + " " + O[2] + " " + P[2] + " " + S[2],
+                O[3] + " " + O[3] + " " + P[3] + " " + S[3],
+                O[4] + " " + O[4] + " " + P[4] + " " + S[4],
+                O[5] + " " + O[5] + " " + P[5] + " " + S[5],
+                O[6] + " " + O[6] + " " + P[6] + " " + S[6]
+        };
+
+        // Enhanced for-loop rendering
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
